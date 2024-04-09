92b503f393f8cc47174f6966ec5fb5bc9c8d6fb5
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-09 10:57:12 +0100

    yh

--- shareme__frontend/src/index.js
+++ shareme__frontend/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import from react-dom/client
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import App from './App'
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Create a root element using createRoot
-const root = createRoot(document.getElementById('root'));
-
-// Render the app inside the root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <GoogleOAuthProvider clientId='1079988461729-bduh232b58g2omn7pkatkde6e4ng6fc9.apps.googleusercontent.com'>
-     <App /> 
-    </GoogleOAuthProvider>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
