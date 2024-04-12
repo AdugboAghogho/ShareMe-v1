777ba535c90cfc78c6d78b9610d42c4cbece7d6b
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-12 09:46:49 +0100

    login stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+  
+      if (!user) navigate('/login');
+    }, []);
+  
     return (
         <Routes>
             <Route path="login" element={<Login />} />
